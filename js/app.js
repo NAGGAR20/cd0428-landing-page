83e5983963ed7b8b288603986e0609c951bf6881
NAGGAR20 <121048988+NAGGAR20@users.noreply.github.com>
2022-12-25 00:02:20 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -59,4 +59,35 @@
 
 // Set sections as active
 
-
+document.addEventListener("DOMContentLoaded",buildnav)
+const fragment = document.createDocumentFragment();
+const list = document.getElementById("navbar__list");
+function buildnav(){
+  const all = document.querySelectorAll("section");
+  for(const section of sections){
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `<a href="#${section.id}" class = "menu__link">${section.getAttribute("data-nav")}`
+    listItem.innerHTML.classList.add("menu__link");
+    listItem.innerHTML.addEventListener("click",evt=>{
+      evt.preventDefault()
+      section.scrollIntoView({
+        behavior:"smooth"
+      })
+    })
+    fragment.appendChild(listItem)
+  }
+  list.appendChild(fragment)
+  
+document.addEventListener("scroll",addHighLight)
+  function addHighLight(){
+for(const section of sections) {
+  const top= section.getBoundingClientRect().top
+  const title = section.getAttribute("data-nav");
+  if(top>0 && top<250){
+    section.classList.add("your-active-class")
+  }else{
+    section.classList.remove("your-active-class")
+   }
+  } 
+ }
+}
